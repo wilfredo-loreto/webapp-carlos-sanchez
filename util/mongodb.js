982c191e463d800d7359383ba845674f8628dcab
Wilfredo Loreto <pipeloreto99@gmail.com>
2021-02-08 11:38:40 -0400

    New options mongod

--- util/mongodb.js
+++ util/mongodb.js
@@ -34,6 +34,9 @@ export async function connectToDatabase() {
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: false,
+      socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30000
     }
 
     cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
